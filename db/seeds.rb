ed43db7813c6f7027c91418bcfa68facfd60c00f
gv4383 <gregory.vargas18@gmail.com>
2020-05-03 22:59:33 -0500

    Create seeds for products table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+products = Product.create([
+  {
+    name: 'Left Boot',
+    price: 1.29
+  },
+  {
+    name: 'Green Mitten',
+    price: 8.69
+  }
+])
