497c605cad7494aeab9dd16ab0a50f9dd834d7b3
gv4383 <gregory.vargas18@gmail.com>
2020-05-05 00:22:59 -0500

    Added product#create method

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,15 +1,27 @@
 module Api
   class ProductsController < ApplicationController
+    protect_from_forgery with: :null_session
+
     def show
       product = Product.find(params[:id])
 
       render json: ProductSerializer.new(product).serialized_json
     end
 
+    def create
+      product = Product.new(product_params)
+
+      if product.save
+        head :no_content
+      else
+        render json: { error: product.errors.messages }, status: 400
+      end
+    end
+
     private
 
     def product_params
-      params.require(:produc).permit(:name, :price)
+      params.require(:product).permit(:name, :price)
     end
   end
 end
